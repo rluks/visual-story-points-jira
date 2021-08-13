b415bf9f07f5d336d2566e907a038ead80f2d5c5
Roman <rluks@seznam.cz>
2021-08-13 12:02:37 +0200

    stylesheet instead of inline js

--- inject.js
+++ inject.js
@@ -8,26 +8,18 @@ function InjectStoryPoints(node, headingNode){
     let catSP = 2;
     let dogSP = 5;
 
-    let baseHeight = 40;
-    let smallHeight = baseHeight + 'px';
-    let mediumHeight = 1.618 * baseHeight + 'px';
-    let bigHeight = 1.618 * 1.618 * baseHeight + 'px';
-
     console.log(node);
     var inputEl = node.getElementsByTagName("input")[0];
 
     var containerEL = document.createElement("div");
     containerEL.setAttribute("id", "visual-sps-jira");
     containerEL.classList.add("visual-story-points-jira");
-    //containerEL.style.width = '100%';
-    //containerEL.style.textAlign = 'center';
 
     headingNode.parentNode.insertBefore(containerEL, headingNode.nextSibling);
 
     var small = document.createElement("button");
     var smallTxt = document.createTextNode("🐭(" + mouseSP + ")");
-    small.style.height = smallHeight;
-    small.style.fontSize = "large";
+    small.classList.add("visual-story-points-jira-button");
     small.appendChild(smallTxt);
     small.onclick = function () {
         inputEl.value = mouseSP;
@@ -36,8 +28,7 @@ function InjectStoryPoints(node, headingNode){
 
     var medium = document.createElement("button");
     var mediumTxt = document.createTextNode("🐱(" + catSP + ")");
-    medium.style.height = mediumHeight;
-    medium.style.fontSize = "large";
+    medium.classList.add("visual-story-points-jira-button");
     medium.appendChild(mediumTxt);
     medium.onclick = function () {
         inputEl.value = catSP;
@@ -46,8 +37,7 @@ function InjectStoryPoints(node, headingNode){
 
     var big = document.createElement("button");
     var bigTxt = document.createTextNode("🐶(" + dogSP + ")");
-    big.style.height = bigHeight;
-    big.style.fontSize = "large";
+    big.classList.add("visual-story-points-jira-button");
     big.appendChild(bigTxt);
     big.onclick = function () {
         inputEl.value = dogSP;
