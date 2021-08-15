2fd5770bdd18715e2c9ec4d8dcdea9b975b585b3
rluks <rluks+x@pm.me>
2021-08-15 15:28:46 +0200

    checking testing URL in content.js thus not requiring code change for publishing

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 "use strict";
 
-/*
+const testURL = "http://localhost:8000/test.html";
+
 function Test(){
     var node = document.getElementById('modal-field-view');
     node.childNodes.forEach((childNode) => {
@@ -14,44 +15,42 @@ function Test(){
     });
 }
 
-setTimeout(Test, 400);
-*/
+if(window.location.href === testURL){
+    setTimeout(Test, 400);
+}else{
 
+    // Select the node that will be observed for mutations
+    const targetNode = document.getElementById('jira');
 
-// Select the node that will be observed for mutations
-const targetNode = document.getElementById('jira');
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: false, childList: true, subtree: false };
 
-// Options for the observer (which mutations to observe)
-const config = { attributes: false, childList: true, subtree: false };
-
-// Callback function to execute when mutations are observed
-const callback = function(mutationsList, observer) {
-    for(const mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            mutation.addedNodes.forEach( (node) => {
-               if(node.id === 'modal-field-view'){
-                   node.childNodes.forEach((childNode) => {
-                       if(childNode.className === 'jira-dialog-heading'){
-                           if(childNode.textContent.includes("Edit Story Points for")){
-                               //console.log("It's a dialogue window for editing story points!");
-                               //console.log(childNode);
-                               InjectStoryPoints(node, childNode);
-                           }
-                       }
-                   });
-               }
-            });
+    // Callback function to execute when mutations are observed
+    const callback = function(mutationsList, observer) {
+        for(const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                mutation.addedNodes.forEach( (node) => {
+                if(node.id === 'modal-field-view'){
+                    node.childNodes.forEach((childNode) => {
+                        if(childNode.className === 'jira-dialog-heading'){
+                            if(childNode.textContent.includes("Edit Story Points for")){
+                                //console.log("It's a dialogue window for editing story points!");
+                                //console.log(childNode);
+                                InjectStoryPoints(node, childNode);
+                            }
+                        }
+                    });
+                }
+                });
+            }
         }
-    }
-};
+    };
 
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
 
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
 
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
-
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
-
-
+}
 
